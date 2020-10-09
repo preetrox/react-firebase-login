937675cc6f6d413afb20155cc181cf40d22de425
Gurpreet <kaurgurpreet283@gmail.com>
2020-10-09 12:49:22 +0530

    final code

---
+++ precache-manifest.0312b63ac7048d2d1ce04f3696044697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b33e20c8e862f5bd0d1ea29bab6a383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb978cf9c69988ca86a0",
+    "url": "/static/css/main.4900b4e1.chunk.css"
+  },
+  {
+    "revision": "2d6ec0b869fdf0f62be5",
+    "url": "/static/js/2.111a11fb.chunk.js"
+  },
+  {
+    "revision": "baf8e19b81885464d04568dc57ed0d6b",
+    "url": "/static/js/2.111a11fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb978cf9c69988ca86a0",
+    "url": "/static/js/main.d2f883be.chunk.js"
+  },
+  {
+    "revision": "1b35a5395f9335667ae7",
+    "url": "/static/js/runtime-main.352d5d07.js"
+  }
+]);
